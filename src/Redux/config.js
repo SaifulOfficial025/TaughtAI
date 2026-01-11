da80a5a7665ff891da3f85528480b87ff6aaa06b
Md Saiful Islam Rimon <saifulofficial025@gmail.com>
2026-01-11 15:16:41 +0600

    baseurl fixing

--- src/Redux/config.js
+++ src/Redux/config.js
@@ -1,4 +1,4 @@
 // Centralized API configuration
-export const BASE_URL = "https://taughtai.app/";
+export const BASE_URL = "https://taughtai.app";
 
 // You can add other config values here in the future, e.g. timeouts, feature flags
